37c184b1a84d897272544194382429315f5812bf
shirdha <46441115+shirdha@users.noreply.github.com>
2019-01-08 17:33:00 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,5 +1,10 @@
 n=int(input(""))
 a=0
+c=0
 for i in range(1,n+1):
     a=n*i
-    print(a,end=" ")
+    if(c==0):
+            print(a,end="")
+            c=c+1
+    else:
+            print("",a,end="")
