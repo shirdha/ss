f58f776f89c56a68e20ce821d596cd49e9d0ddd3
shirdha <46441115+shirdha@users.noreply.github.com>
2019-01-08 16:06:11 +0530

    Create oddintr.py

---
+++ oddintr.py
@@ -0,0 +1,9 @@
+n,q=map(int,input().split())
+c=0
+for i in range(n+1,q):
+    if(i%2!=0):
+        if(c==0):
+            print(i,end="")
+            c=c+1
+        else:
+            print("",i,end="")
