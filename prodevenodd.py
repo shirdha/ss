22d3bdbd5e048497eec1883b08ec119f23451282
shirdha <46441115+shirdha@users.noreply.github.com>
2019-01-22 22:25:30 +0530

    Update prodevenodd.py

--- prodevenodd.py
+++ prodevenodd.py
@@ -1,6 +1,6 @@
-n,m=map(int,input().split())
-a=n*m
+n,m=map(int,input().split()) #get the ip 
+a=n*m #product of n and m
 if(a%2==0):
     print("even") #print even
 else:
-    print("odd")#print odd
+    print("odd") #print odd
