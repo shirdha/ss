f578e4a852d7bfaa3a6f44572e47206864ba0e24
shirdha <46441115+shirdha@users.noreply.github.com>
2019-01-30 22:30:07 +0530

    Update odd_factor.py

--- odd_factor.py
+++ odd_factor.py
@@ -1,7 +1,12 @@
 n=int(input())
 val=1
+c=0
 while(val<=n):
     if n%val==0:
-        if n%2!=0:
-            print(format(val),end=" ") 
+        if val%2==1:
+            if c==0:
+                print(val,end="")
+                c+=1
+            else:
+                print("",val,end="")
     val=val+1
