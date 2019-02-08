637ce0d126466467e55dd7849412027337387375
shirdha <46441115+shirdha@users.noreply.github.com>
2019-02-08 19:37:13 +0530

    Update no_present_in_array.py

--- no_present_in_array.py
+++ no_present_in_array.py
@@ -2,7 +2,7 @@ n,k=map(int,input().split())
 N=list(map(int,input().split()))
 for i in N:
     if i==k:
-        print("yes")
+        print("Yes")
         break
 else:
-    print("no")
+    print("No")
